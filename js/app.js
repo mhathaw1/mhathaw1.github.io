3cb1300104c3dd82cef5390bf1aeb8144195c12f
Michael <mhathaw1@gmail.com>
2019-10-18 18:17:55 -0400

    finalized, if not particularly pretty looking, portfolio site submission for grading

--- js/app.js
+++ js/app.js
@@ -42,12 +42,13 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
 }
 
+//Menu!
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
 
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+  if (!event.target.matches('.blueButton')) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
@@ -60,27 +61,17 @@ window.onclick = function(event) {
 }
   
 //Modal time!
-
-// Get the modal
 var modal = document.getElementById("practiceModal");
-
-// Get the button that opens the modal
 var btn = document.getElementById("modalButton");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
 btn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
